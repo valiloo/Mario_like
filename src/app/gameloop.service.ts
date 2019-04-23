1fc4c6f955890764d8c9f73ddeed8909332a8207
Valentin Pawlowski <valentinp821@gmail.com>
2019-04-23 10:19:06 +0200

    ok

--- src/app/gameloop.service.ts
+++ src/app/gameloop.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, Output, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { GamestateService, MOVE_RIGHT, MOVE_LEFT, MOVE_FORWARD, MOVE_BACKWARD, MOVE_UPWARD } from './gamestate.service';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { MapTheme, MapService, VI } from './map.service';
 
 
@@ -12,7 +11,7 @@ import { MapTheme, MapService, VI } from './map.service';
 export class GameloopService {
 
   public canGoThrough
-  public canMove
+
 
 
   public jump: number = 0
@@ -20,71 +19,63 @@ export class GameloopService {
   public xAxis: number = 0
   public yAxis: number = 0
   public scaleX: number
+  public innerWidth: number
 
-  constructor(public gameService: GamestateService,public mapTheme: MapTheme, public mapService: MapService) { }
-
-  start(): any {
-
-    this.canMove = () => {
-
-          if (this.jump > 0) {
-
-            this.jump -= 1
-
-          }
-
-          if (this.yAxis < 0){
+  constructor(public gameService: GamestateService, public mapTheme: MapTheme, public mapService: MapService) { }
 
 
-            this.yAxis += 4
 
-          }
+  public canMove() {
 
-          if (((this.gameService.move === MOVE_RIGHT) || (this.gameService.move === MOVE_LEFT)) && this.gameService.xVelocity === MOVE_FORWARD) {
+    if (this.jump > 0) {
 
-            this.scaleX = -1
-            this.xAxis += 10
-            this.move = 1
+      this.jump -= 1
 
-
-      }
-
-          if (((this.gameService.move === MOVE_RIGHT) || (this.gameService.move === MOVE_LEFT)) && this.gameService.xVelocity === MOVE_BACKWARD) {
-
-            this.scaleX = 1
-            this.xAxis -= 3
-            this.move = 1
-
-      }
-
-          if (this.gameService.yVelocity === MOVE_UPWARD){
-
-            this.jump = 45
-            this.yAxis -= 230
-            this.gameService.yVelocity = 0
-
-          }
-
-          else if ((this.gameService.move !== MOVE_RIGHT) && (this.gameService.move !== MOVE_LEFT)) {
-
-           this.move = 0
-
-      }
-      
     }
-  
-    this.canMove()
-    
-    this.canGoThrough = () => {
 
-    for(let x= 0; x < this.mapService.map.length ; x++){
-      for(let y= 0; y < this.mapService.map[x].length; y++){
+    if (this.yAxis < 0) {
 
-        if(this.mapTheme.textures[y] === VI){
-            this.yAxis +=5
-        }
-      }
+
+      this.yAxis += 4
+
     }
+
+    if (((this.gameService.move === MOVE_RIGHT) || (this.gameService.move === MOVE_LEFT)) && this.gameService.xVelocity === MOVE_FORWARD) {
+
+      this.scaleX = -1
+      this.xAxis += 10
+      this.move = 1
+
+
     }
+
+    if (((this.gameService.move === MOVE_RIGHT) || (this.gameService.move === MOVE_LEFT)) && this.gameService.xVelocity === MOVE_BACKWARD) {
+
+      this.scaleX = 1
+      this.xAxis -= 3
+      this.move = 1
+
+    }
+
+    if (this.gameService.yVelocity === MOVE_UPWARD) {
+
+      this.jump = 45
+      this.yAxis -= 230
+      this.gameService.yVelocity = 0
+
+    }
+
+    else if ((this.gameService.move !== MOVE_RIGHT) && (this.gameService.move !== MOVE_LEFT)) {
+
+      this.move = 0
+
+    }
+
   }
+
+  start() {
+    this.canMove()
+    requestAnimationFrame(() => this.canMove())
+  }
+
 }
