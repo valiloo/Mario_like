4f1aaf2b05a8dbf5a276579b7016b8f179ca4ffe
Emilie Barlet <emiliegrimoud@yahoo.fr>
2019-04-24 09:28:11 +0200

    ogre

--- src/app/models/ogr.ts
+++ src/app/models/ogr.ts
@@ -8,6 +8,6 @@ export class OgrMonster extends Ogr {
     
     constructor(public initPosX: number, initPosY: number, public amplitude: number = 2, public direction = 1 /* MOVE_RIGHT */) {
 
-        super('/assets/Sprites/os3.png', initPosX, initPosY)
+        super('/assets/Sprites/ogr.png', initPosX, initPosY)
     }
 }
